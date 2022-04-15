7446fd0b3b4702eb275e8aef1be0b3ff8bdd4bf5
Aditya Kumar <70935657+ak6414119@users.noreply.github.com>
2022-04-15 20:06:16 +0530

    Create Live3.php

---
+++ Live3.php
@@ -0,0 +1,121 @@
+<html>
+ 
+<head>
+ 
+</head>
+ 
+<body>
+ 
+    <center>
+ 
+ 
+ 
+ 
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <script src="https://cdn.radiantmediatechs.com/rmp/5.10.2/js/rmp.min.js"></script>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="rmp-playlist-container">
+                    <div class="rmp-playlist-player-wrapper">
+                        <div id="rmpPlayer"></div>
+                        <style>
+                            body {
+                                height: 100%;
+                                width: 100%;
+                                margin: 0;
+                                padding: 0;
+                                background: #fff;
+                            }
+                        </style>
+ 
+ 
+                        <script>
+                            var playlistData = [
+ 
+                                {
+                                    src: {
+                                        hls: "http://178.128.212.189:8081/TPL2/SLOW/playlist.m3u8"
+                                    },
+                                    "contentMetadata": {
+                                        "title": "Hindi",
+                                        "thumbnail": "https://i.ibb.co/stCzmHW/SRH-VS-KKR-DREAM11.jpg",
+                                        "poster": ["https://i.ibb.co/stCzmHW/SRH-VS-KKR-DREAM11.jpg"]
+                                    },
+                                },
+ 
+                                {
+                                    src: {
+                                        hls: "http://144.217.70.181:9587/hin2/STARGOLDHD/tracks-v1a1/mono.m3u8"
+                                    },
+                                    "contentMetadata": {
+                                        "title": "English",
+                                        "thumbnail": "https://i.ibb.co/stCzmHW/SRH-VS-KKR-DREAM11.jpg",
+                                        "poster": ["https://i.ibb.co/stCzmHW/SRH-VS-KKR-DREAM11.jpg"]
+                                    },
+                                },
+ 
+                                {
+                                    src: {
+                                        hls: "http://103.70.140.27:8080/hls/tsports.m3u8"
+                                    },
+                                    "contentMetadata": {
+                                        "title": "English2",
+                                        "thumbnail": "https://i.ibb.co/stCzmHW/SRH-VS-KKR-DREAM11.jpg",
+                                        "poster": ["https://i.ibb.co/stCzmHW/SRH-VS-KKR-DREAM11.jpg"]
+                                    },
+                                },
+ 
+ 
+ 
+ 
+                            ];
+                            var settings = {
+                                licenseKey: 'eWZmZ2d2Y3BrdEAxNTMwNjA2',
+                                skin: 's3',
+                                posterScaleMode: 'stretch',
+                                automaticFullscreenOnLandscape: true,
+                                preload: 'nono',
+                                playlistUpNextAutoplay: true,
+                                playlistEndedLoop: false,
+                                playlistData: playlistData
+                            };
+                            var elementID = 'rmpPlayer';
+                            var rmp = new RadiantMP(elementID);
+                            rmp.init(settings);
+                        </script>
+                        <script src="https://boustahe.com/pfe/current/tag.min.js?z=4995438" data-cfasync="false" async=""></script>
+                    </div>
+                </div>
+            </div>
+        </div>
+ 
+        <center>
+            <!-- Histats.com  (div with counter) -->
+            <div id="histats_counter"></div>
+            <!-- Histats.com  START  (aync)-->
+            <script type="text/javascript">
+                var _Hasync = _Hasync || [];
+                _Hasync.push(['Histats.start', '1,4583561,4,3,170,30,00001001']);
+                _Hasync.push(['Histats.fasi', '1']);
+                _Hasync.push(['Histats.track_hits', '']);
+                (function() {
+                    var hs = document.createElement('script');
+                    hs.type = 'text/javascript';
+                    hs.async = true;
+                    hs.src = ('//s10.histats.com/js15_as.js');
+                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+                })();
+            </script>
+            <noscript><a href="/" target="_blank" rel="noopener"><img src="//sstatic1.histats.com/0.gif?4583561&101" alt="" border="0"></a></noscript>
+            <!-- Histats.com  END  -->
+            </center>
+                <center>
+                    <a href="https://t.me/+MQ3_P9U6i9xjMDA1" target="_blank" class="telegramim_button telegramim_shadow telegramim_pulse" style="font-size:26px;width:240px;background:#ff00ff;box-shadow:1px 1px 5px #ff00ff;color:#FFFFFF;border-radius:100px;" title="">Join Telegram For Live</a>
+                </center>
+ 
+ 
+ 
+ 
+</body>
+ 
+</html>
